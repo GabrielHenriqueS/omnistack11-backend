3bc414f94fd295b82916dd33458fe46c1e58d73d
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2020-03-24 22:44:42 -0400

    Week 11 day 2

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.app = express();
+
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(routes);
+  }
+}
+
+export default new App().app;
